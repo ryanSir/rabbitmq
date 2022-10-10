f7b69d53d9fa566a70f577619891bbba0a48e664
Ryan <yang.zhao@oceanus-plus.com>
2022-10-10 18:04:58 +0800

    消息中间件简单实现

--- src/main/java/com/ryan/rabbitmq/quickstartdirect/ConsumerDirect.java
+++ src/main/java/com/ryan/rabbitmq/quickstartdirect/ConsumerDirect.java
@@ -1,6 +1,9 @@
-package com.ryan.rabbitmq.quickstart;
+package com.ryan.rabbitmq.quickstartdirect;
 
-import com.rabbitmq.client.*;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
@@ -9,7 +12,7 @@ import java.util.concurrent.TimeoutException;
  * @author ryan
  * @version Id: Consumer, v 0.1 2022/10/10 1:53 PM ryan Exp $
  */
-public class Consumer {
+public class ConsumerDirect {
 
     public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
         //1. 创建连接工厂
@@ -18,15 +21,27 @@ public class Consumer {
         connectionFactory.setPort(5672);
         connectionFactory.setVirtualHost("my_rabbit");
 
+        // 是否支持重连，每三秒执行一次
+        connectionFactory.setAutomaticRecoveryEnabled(true);
+        connectionFactory.setNetworkRecoveryInterval(3000);
+
         //2. 通过链接工厂创建连接
         Connection connection = connectionFactory.newConnection();
 
         //3. 通过connection创建一个Channel
         Channel channel = connection.createChannel();
 
-        //4. 声明一个队列,启动后会自动添加到管控台   durable:是否持久化，ture,即使服务器重启，队列也不会被消失， exclusive：独占，ture,顺序消费 autoDelete:脱离了exchange，会自动删除
-        String queueName = "test001";
-        channel.queueDeclare(queueName, true, false, false, null);
+        //4. 声明
+        String exchangeName = "test_direct_exchange";
+        String exchangeType = "direct";
+        String queueName = "test_direct_queue";
+        String routingKey = "test.direct";
+        // 声明交换机
+        channel.exchangeDeclare(exchangeName,exchangeType,true,false,false,null);
+        // 声明队列 durable:是否持久化，ture,即使服务器重启，队列也不会被消失， exclusive：独占，ture,顺序消费 autoDelete:脱离了exchange，会自动删除
+        channel.queueDeclare(queueName, false, false, false, null);
+        // 建立绑定关系
+        channel.queueBind(queueName,exchangeName,routingKey);
 
         //5. 创建消费者
         QueueingConsumer queueingConsumer = new QueueingConsumer(channel);
---
+++ src/main/java/com/ryan/rabbitmq/quickstarttopic/ProducerTopic.java
@@ -0,0 +1,50 @@
+package com.ryan.rabbitmq.quickstarttopic;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * exchange direct 模式
+ *
+ * @author ryan
+ * @version Id: Procuder, v 0.1 2022/10/10 1:53 PM ryan Exp $
+ */
+public class ProducerTopic {
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        //1. 创建连接工厂
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("20.50.54.194");
+        connectionFactory.setPort(5672);
+        connectionFactory.setVirtualHost("my_rabbit");
+
+        //2. 通过链接工厂创建连接
+        Connection connection = connectionFactory.newConnection();
+
+        //3. 通过connection创建一个Channel
+        Channel channel = connection.createChannel();
+
+        //4. 声明
+        String exchangeName = "test_topic_exchange";
+        String routingKey1 = "user.save";
+        String routingKey2 = "user.update";
+        String routingKey3 = "user.delete.abc";
+
+        //5. 发送
+        for (int i = 0; i < 1000; i++) {
+            String message = "hello rabbitmq for topic exchange message!";
+            channel.basicPublish(exchangeName, routingKey1, null, message.getBytes());
+            channel.basicPublish(exchangeName, routingKey2, null, message.getBytes());
+            channel.basicPublish(exchangeName, routingKey3, null, message.getBytes());
+        }
+
+
+        //5. 记得要关闭连接
+        channel.close();
+        connection.close();
+    }
+}
